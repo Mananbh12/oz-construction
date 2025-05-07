1d14f3b02cfe0e8ceb42f1387ac6bcf0130e00d0
Arya Putra <mananbhrdwj@gmail.com>
2025-05-07 14:28:19 +0200

    first commit

---
+++ oz-sanity/schemaTypes/project.ts
@@ -0,0 +1,41 @@
+import { defineType, defineField } from 'sanity';
+
+export default defineType({
+  name: 'project',
+  title: 'Projets',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Titre',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{ type: 'image', options: { hotspot: true } }],
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+    }),
+  ],
+});
