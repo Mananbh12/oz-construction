87cadabb4c5c1a349140265e50b249db4e9fb4b7
Arya Putra <mananbhrdwj@gmail.com>
2025-05-18 17:14:12 +0200

    push vercel

--- oz/app/projects/[slug]/page.tsx
+++ oz/app/projects/[slug]/page.tsx
@@ -65,7 +65,7 @@ export default async function ProjectDetail({ params }: PageProps) {
           <strong>Completed on:</strong>{' '}
           {new Date(project.date).toLocaleDateString()}
         </p>
-      )}
+      )} 
       <a
         href="/projects"
         className="text-yellow-500 hover:text-gray-700 mt-4 inline-block"
